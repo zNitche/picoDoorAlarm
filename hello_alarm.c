465155baa162a4e7dbef13f243745e3d415c1740
zNitche <53448802+zNitche@users.noreply.github.com>
2026-01-16 22:06:25 +0100

    feat: updated example with support for onboard led blink

--- hello_alarm.c
+++ hello_alarm.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
-int main() {
+void set_led(bool led_on)
+{
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+}
+
+int main()
+{
     stdio_init_all();
 
-    while (true) {
+    int rc = cyw43_arch_init();
+    hard_assert(rc == PICO_OK);
+
+    bool led_state = false;
+
+    while (true)
+    {
         printf("Hello Pico Door Alarm 123!\n");
+        
+        led_state = !led_state;
+        set_led(led_state);
+
         sleep_ms(1000);
     }
 }
